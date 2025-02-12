6b599ffa38eb004cf96bb5e2e692d398ac526912
Ansar Afsar <83577572+Ansarafsar@users.noreply.github.com>
2025-02-12 23:06:02 +0530

    data_loader.py

---
+++ utils/data_loader.py
@@ -0,0 +1,9 @@
+# utils/data_loader.py
+import pandas as pd
+
+def load_data():
+    """
+    Load the cleaned and processed dataset.
+    """
+    data_path = "data\pp_new_data.csv"
+    return pd.read_csv(data_path)
